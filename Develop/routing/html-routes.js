96b37ef6a7ee0eaf8d2df32b97fbf5d85fafc152
EJostad <eric.jostad@outlook.com>
2021-02-01 23:11:28 -0800

    Small changes to index.html & html-routes files. Server.js now loaded index.html via local host :)

--- Develop/routing/html-routes.js
+++ Develop/routing/html-routes.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = function (app) {
     
     app.get('/', function(req, res){
-        res.sendFiles(path.join(__dirname + '/Develop/assets/html/index.html'));
+        res.sendFile(path.join(__dirname + '/../assets/html/index.html'));
     });
 
     app.get('/notes', function(req, res){
-        res.sendFiles(path.join(__dirname + '/Develop/assets/html/notes.html'));
+        res.sendFile(path.join(__dirname + '/../assets/html/notes.html'));
     });
 }
--- Develop/server.js
+++ Develop/server.js
@@ -6,21 +6,12 @@ const path = require('path');
 const app = express();
 const PORT = 1170;
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.use(function (req, res) {
-    res.setHeader('Content-Type', 'text/plain')
-    res.write('you posted:\n')
-    res.end(JSON.stringify(req.body, null, 2))
-})
+// This requires the server to route the user to the html index file 
+require('./routing/html-routes')(app);
 
 // Listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
