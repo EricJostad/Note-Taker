e059e636adad69840a175fafa00fbf5b6acb212c
EJostad <eric.jostad@outlook.com>
2021-02-01 21:15:09 -0800

    Updated server file to now connect with local host 1170.

--- Develop/server.js
+++ Develop/server.js
@@ -6,5 +6,20 @@ const app = express();
 const PORT = 1170; 
 
 app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
 
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+
+app.use(function (req, res) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.write('you posted:\n')
+    res.end(JSON.stringify(req.body, null, 2))
 })
+
+// Listener
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
