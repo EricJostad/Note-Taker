2f385e9d748450df8998336ee3616668ec66e6a3
EJostad <eric.jostad@outlook.com>
2021-02-01 20:59:54 -0800

    Adding initial code to server.js file. Installed express and body-parser modules.

---
+++ Develop/server.js
@@ -0,0 +1,10 @@
+// Setting up required dependencies
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+const PORT = 1170; 
+
+app.get('/', function (req, res) {
+
+})
