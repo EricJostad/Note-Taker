228fd5fd76e748f49edebbb57330f36588ebfba1
EJostad <eric.jostad@outlook.com>
2021-02-03 23:31:58 -0800

    Tweaked bug fix on index.js file. Updated api-routes.js file to include post code.

--- Develop/routing/api-routes.js
+++ Develop/routing/api-routes.js
@@ -5,4 +5,8 @@ module.exports = function (app) {
     app.get('/api/notes', function (req, res) {
         res.json(db.json);
     })
+
+    app.post('/api/notes', function (req, res) {
+        db.json.push(req.body);
+    })
 }
