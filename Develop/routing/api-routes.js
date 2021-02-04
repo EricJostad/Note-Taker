750bf66b0583708789124e70b204bf59f2362b00
EJostad <eric.jostad@outlook.com>
2021-02-04 00:11:07 -0800

    Small change to index.js file. Installed generate-unique-id package. Fleshed out further code into api-routes.js file

--- Develop/routing/api-routes.js
+++ Develop/routing/api-routes.js
@@ -1,12 +1,54 @@
+// Setting up all required modules
 const noteData = require('../db/db.json');
+const path = require('path');
+const fs = require('fs');
+const generateUniqueId = require('generate-unique-id');
+
+require('./html-routes');
 
 module.exports = function (app) {
 
-    app.get('/api/notes', function (req, res) {
-        res.json(db.json);
+    fs.readFile('db/db.json', 'utf8', function (err, data) {
+        if (err) throw err;
+
+        let notes = JSON.parse(data);
+
+        app.get('/api/notes', function (req, res) {
+            res.json(notes);
+        })
+
+        app.post('/api/notes', function (req, res) {
+
+            const activeNote = {
+                title: req.body.title,
+                text: req.body.text,
+                id: generateUniqueId({
+                    length: 10,
+                    useLetters: true,
+                    useNumbers: true
+                })
+            };
+            notes.push(activeNote);
+            updateNotes(notes);
+            res.json(notes);
+        })
+
+        app.get('/api/notes/:id', function (req, res) {
+            res.json(notes[req.params.id]);
+        })
+
+        app.delete('/api/notes/:id', function (req, res) {
+            notes.splice(req.params.id, 1);
+            updateNotes(notes);
+            res.json(notes);
+        })
     })
 
-    app.post('/api/notes', function (req, res) {
-        db.json.push(req.body);
-    })
+    // update json file when notes are added or deleted
+    function updateNotes(notes) {
+        fs.writeFile('db/db.json', JSON.stringify(notes, '\t'), err => {
+            if (err) throw err;
+            return true;
+        });
+    }
 }
