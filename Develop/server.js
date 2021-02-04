cdd2e1a1344af88e4ac6dc255eff76086a541692
EJostad <eric.jostad@outlook.com>
2021-02-03 23:43:12 -0800

    Updated server.js file to include middleware setup, as well as additional comments.

--- Develop/server.js
+++ Develop/server.js
@@ -3,12 +3,16 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 
+// This creates the express app
 const app = express();
 const PORT = 1170;
 
+// Setting up middleware to parse json file
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('./public'));
 
 require('./routing/api-routes')(app);
 require('./routing/html-routes')(app);
