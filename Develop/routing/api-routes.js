7fed450f8563ccf9364f1767559c1b3bb6fd5e7d
EJostad <eric.jostad@outlook.com>
2021-02-05 23:36:47 -0800

    Updated notes and format in api-routes & html-routes files.

--- Develop/routing/api-routes.js
+++ Develop/routing/api-routes.js
@@ -8,35 +8,40 @@ require('./html-routes');
 
 module.exports = function (app) {
 
+    // This sets the creation of the notes variable
     fs.readFile('db/db.json', 'utf8', function (err, data) {
         if (err) throw err;
 
-        let notes = JSON.parse(data);
+        const notes = JSON.parse(data);
 
+        // This will get the route for /api/notes
         app.get('/api/notes', function (req, res) {
             res.json(notes);
         })
 
+        // This will set the post route for /api/notes
         app.post('/api/notes', function (req, res) {
 
-            const activeNote = {
+            const currentNote = {
                 title: req.body.title,
                 text: req.body.text,
                 id: generateUniqueId({
-                    length: 10,
+                    length: 7,
                     useLetters: true,
                     useNumbers: true
                 })
             };
-            notes.push(activeNote);
+            notes.push(currentNote);
             updateNotes(notes);
             res.json(notes);
         })
 
+        // This will get note with unique ID
         app.get('/api/notes/:id', function (req, res) {
             res.json(notes[req.params.id]);
         })
 
+        // This will delete a note with unique ID
         app.delete('/api/notes/:id', function (req, res) {
             notes.splice(req.params.id, 1);
             updateNotes(notes);
@@ -44,7 +49,7 @@ module.exports = function (app) {
         })
     })
 
-    // update json file when notes are added or deleted
+    // This will update json file when notes are added or deleted
     function updateNotes(notes) {
         fs.writeFile('db/db.json', JSON.stringify(notes, '\t'), err => {
             if (err) throw err;
--- Develop/routing/html-routes.js
+++ Develop/routing/html-routes.js
@@ -2,13 +2,13 @@
 const path = require('path');
 
 module.exports = function (app) {
-    
+
     // These lines route the user to the needed html file based on url
-    app.get('/', function(req, res){
+    app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname + '/../public/index.html'));
     });
 
-    app.get('/notes', function(req, res){
+    app.get('/notes', function (req, res) {
         res.sendFile(path.join(__dirname + '/../public/notes.html'));
     });
 }
